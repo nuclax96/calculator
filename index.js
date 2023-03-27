9e4a30826fd3a329fa1677a796d63897d2a8187a
nuclax96 <adityarastogi94c@gmail.com>
2023-03-27 14:13:54 +0530

    Making stuff work wiring up JS , digits and operator buttons working

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Variables Decleration
-
-const inpurArr = [];
-
+let result = "";
+let operand1 = "",
+  operand2 = "";
+let operator;
+let currentOperand = 1;
 // Query Selectors
 const screenOperationText = document.querySelector(".screen-operation");
 
@@ -9,21 +11,79 @@ const screenOperationText = document.querySelector(".screen-operation");
 const keysBtn = document.querySelector(".keypad");
 
 // Functions
+//Assign Operator
+const assignOperator = (op) => {
+  if (op.textContent === "=") {
+    console.log("Equal to");
+    calculate();
+    return;
+  }
+  operator = op.textContent;
+  currentOperand = 2;
+  console.log(operator);
+};
 
-// 1. Delete a digit
+//Assign Operand
+
+const assignOperand = (input) => {
+  if (currentOperand === 1) {
+    operand1 = operand1 + input.textContent;
+    console.log("OP1", operand1);
+  }
+  if (currentOperand == 2) {
+    operand2 = operand2 + input.textContent;
+    console.log("OP2", operand2);
+  }
+};
+// Delete a digit
 const deleteDigit = () => {};
 
-// 2. Reset Screen
+// Reset Screen
 const eraseScreen = () => {};
 
-// 3.Key Press
+// Calculate - Perform Operation
+const calculate = () => {
+  result = `${Number(operand1)} ${operator} ${Number(operand2)}`;
+
+  let op1 = Number(operand1);
+  let op2 = Number(operand2);
+  switch (operator) {
+    case "+":
+      console.log("This time add operation");
+      break;
+    case "-":
+      console.log("Subtraction this time");
+      break;
+    case "*":
+      multiply(op1, op2);
+      break;
+    case "/":
+      divide(op1, op2);
+      break;
+    default:
+      console.log("Default");
+  }
+};
+
+//Check Input - if digit or operation
+
+const handleInput = (currInput) => {
+  const inputType = currInput.target;
+  if (inputType.dataset.value == "operator") {
+    assignOperator(inputType);
+  } else {
+    assignOperand(currInput.target);
+  }
+};
+
+// Key Press
 
 const keyPress = (e) => {
-  const x = e.target;
+  const input = e.target.textContent;
   if (e.target.classList[0] === "keypad") {
     return;
   }
-  console.log(e.target.textContent);
+  handleInput(e);
 };
 
 // Event Listeners
